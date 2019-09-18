0bcdf44e2d4b7f181320291ff7371e01baf911d2
kadir4789 <54605659+kadir4789@users.noreply.github.com>
2019-09-18 06:44:46 -0700

    Update GitTest.java

--- src/GitTest.java
+++ src/GitTest.java
@@ -2,7 +2,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("1");
         System.out.println("new 2");
-
+        System.out.println("new 6");
 
     }
 }
