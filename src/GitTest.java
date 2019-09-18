95b0ea4b340406513ec9eb73d96ac7ed9a91d2d2
Kadir <txgumuskadir@gmail.com>
2019-09-18 06:38:52 -0700

    6th commit

--- src/GitTest.java
+++ src/GitTest.java
@@ -2,8 +2,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("1");
         System.out.println("new 2");
-        System.out.println("new 3");
-        System.out.println("new 4");
+
 
     }
 }
